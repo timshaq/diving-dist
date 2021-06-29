6cc4f98e96e48e59e3eb41fa2f9c5d5319ba6c76
testtimshaq <58642417+testtimshaq@users.noreply.github.com>
2021-06-29 11:03:09 +0500

    update 2906

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,12 @@
 
   function n(i) {
     if (t[i]) return t[i].exports;
-    var r = t[i] = {
+    var s = t[i] = {
       i: i,
       l: !1,
       exports: {}
     };
-    return e[i].call(r.exports, r, r.exports, n), r.l = !0, r.exports;
+    return e[i].call(s.exports, s, s.exports, n), s.l = !0, s.exports;
   }
 
   n.m = e, n.c = t, n.d = function (e, t, i) {
@@ -29,9 +29,9 @@
     if (n.r(i), Object.defineProperty(i, "default", {
       enumerable: !0,
       value: e
-    }), 2 & t && "string" != typeof e) for (var r in e) n.d(i, r, function (t) {
+    }), 2 & t && "string" != typeof e) for (var s in e) n.d(i, s, function (t) {
       return e[t];
-    }.bind(null, r));
+    }.bind(null, s));
     return i;
   }, n.n = function (e) {
     var t = e && e.__esModule ? function () {
@@ -42,7 +42,7 @@
     return n.d(t, "a", t), t;
   }, n.o = function (e, t) {
     return Object.prototype.hasOwnProperty.call(e, t);
-  }, n.p = "", n(n.s = 5);
+  }, n.p = "", n(n.s = 8);
 }([function (e, t, n) {
   "use strict";
 
@@ -50,14 +50,14 @@
     function e(e, t, n) {
       const i = e;
       if (!i) return;
-      const r = window.matchMedia(`(min-width:${t}px)`);
-      let s;
+      const s = window.matchMedia(`(min-width:${t}px)`);
+      let c;
 
-      const c = function () {
-        !0 !== r.matches ? !1 !== r.matches || (s = new Swiper(i, n)) : void 0 !== s && s.destroy(!0, !0);
+      const r = function () {
+        !0 !== s.matches ? !1 !== s.matches || (c = new Swiper(i, n)) : void 0 !== c && c.destroy(!0, !0);
       };
 
-      r.addListener(c), c();
+      s.addListener(r), r();
     }
 
     const t = document.querySelectorAll(".services-places .services-places__content-box .swiper-container"),
@@ -68,8 +68,16 @@
         spaceBetween: 10
       });
     });
-    const i = document.getElementById("articleAsideSlider");
-    i && e(i, 1200, {
+    const i = document.querySelectorAll(".index-price .index-price__content-box .swiper-container"),
+          s = Array.prototype.slice.call(i);
+    s.length > 0 && s.forEach(t => {
+      e(t, 768, {
+        slidesPerView: 1.23,
+        spaceBetween: 10
+      });
+    });
+    const c = document.getElementById("articleAsideSlider");
+    c && e(c, 1200, {
       breakpoints: {
         0: {
           slidesPerView: 1.23,
@@ -95,6 +103,178 @@
   "use strict";
 
   n.r(t), t.default = function () {
+    const e = document.getElementById("header");
+
+    function t() {
+      const t = document.documentElement.scrollTop;
+      t > 98 && e.classList.add("active"), t < 98 && e.classList.remove("active");
+    }
+
+    document.addEventListener("scroll", function () {
+      t();
+    }), t();
+  };
+}, function (e, t, n) {
+  "use strict";
+
+  n.r(t), t.default = function () {
+    const e = document.getElementById("indexHeroParallax");
+
+    if (e) {
+      const t = document.getElementById("indexHeroDiver"),
+            n = document.getElementById("indexHeroFishLeft"),
+            i = document.getElementById("indexHeroFishRight"),
+            s = 10,
+            c = 25,
+            r = 15,
+            o = .05;
+      let a = 0,
+          l = 0,
+          d = 0,
+          u = 0;
+      !function e() {
+        a += (d - a) * o, l += (u - l) * o, t.style.cssText = `transform: translate(${a / s - 50}%,${l / s - 50}%)`, n.style.cssText = `transform: translate(${-a / c}%,${-l / c}%)`, i.style.cssText = `transform: translate(${-a / r}%,${-l / r}%)`, requestAnimationFrame(e);
+      }(), e.addEventListener("mousemove", function (t) {
+        const n = e.offsetWidth,
+              i = e.offsetHeight,
+              s = t.pageX - n / 2,
+              c = t.pageY - i / 2;
+        d = s / n * 100, u = c / i * 100;
+      });
+    }
+  };
+}, function (e, t, n) {
+  "use strict";
+
+  n.r(t), t.default = function () {
+    const e = document.getElementById("indexPrice");
+
+    if (e) {
+      const t = e.querySelectorAll(".tabs-box .tab"),
+            n = Array.prototype.slice.call(t),
+            i = e.querySelectorAll(".services-places__content"),
+            s = Array.prototype.slice.call(i);
+      n.forEach((e, t) => {
+        e.addEventListener("click", function () {
+          n.forEach(e => e.classList.remove("tab-active")), e.classList.add("tab-active"), s.forEach(e => e.classList.remove("active")), s[t].classList.add("active");
+        });
+      });
+    }
+
+    const t = document.getElementById("indexTeamLeadToggler");
+
+    if (t) {
+      const e = document.getElementById("indexTeamLeadText");
+      t.addEventListener("click", function () {
+        e.classList.contains("open") ? (e.classList.remove("open"), t.textContent = "Подробнее") : (e.classList.add("open"), t.textContent = "Скрыть");
+      });
+    }
+
+    const n = document.getElementById("indexTeamListSlider");
+
+    if (n) {
+      new Swiper(n, {
+        watchOverflow: !0,
+        pagination: {
+          el: "#indexTeamListSliderPagination",
+          type: "bullets"
+        },
+        breakpoints: {
+          0: {
+            slidesPerView: 1.23,
+            spaceBetween: 10
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 30
+          },
+          1200: {
+            slidesPerView: 4,
+            spaceBetween: 30
+          }
+        }
+      });
+    }
+
+    const i = document.getElementById("indexPlacesSlider");
+
+    if (i) {
+      new Swiper(i, {
+        watchOverflow: !0,
+        pagination: {
+          el: "#indexPlacesSliderPagination",
+          type: "bullets"
+        },
+        navigation: {
+          prevEl: "#indexPlacesSliderPrevBtn",
+          nextEl: "#indexPlacesSliderNextBtn"
+        },
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 10
+          },
+          768: {
+            centeredSlides: !0,
+            loop: !0,
+            slidesPerView: 1.53,
+            spaceBetween: 30
+          },
+          1370: {
+            centeredSlides: !0,
+            loop: !0,
+            slidesPerView: 2.3,
+            spaceBetween: 30
+          }
+        }
+      });
+    }
+
+    const s = document.getElementById("indexTrain");
+
+    if (s) {
+      const e = s.querySelectorAll(".tabs-box .tab"),
+            t = Array.prototype.slice.call(e),
+            n = s.querySelectorAll(".index-train__content"),
+            i = Array.prototype.slice.call(n);
+      t.forEach((e, n) => {
+        e.addEventListener("click", function () {
+          t.forEach(e => e.classList.remove("tab-active")), e.classList.add("tab-active"), i.forEach(e => e.classList.remove("active")), i[n].classList.add("active");
+        });
+      });
+    }
+
+    const c = document.querySelectorAll(".index-train .index-train__content-box .swiper-container"),
+          r = Array.prototype.slice.call(c);
+    r.length > 0 && r.forEach(e => {
+      const t = e.querySelector(".swiper-pagination");
+      new Swiper(e, {
+        watchOverflow: !0,
+        pagination: {
+          el: t,
+          type: "bullets"
+        },
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 10
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30
+          },
+          1200: {
+            slidesPerView: 3,
+            spaceBetween: 30
+          }
+        }
+      });
+    });
+  };
+}, function (e, t, n) {
+  "use strict";
+
+  n.r(t), t.default = function () {
     const e = document.getElementById("menu"),
           t = document.getElementById("burgerBtn"),
           n = document.getElementById("menuCloseBtn");
@@ -103,12 +283,12 @@
       e.classList.remove("open"), html.style.overflowX = "hidden", html.style.overflowY = "scroll";
     }
 
-    function r(e) {
+    function s(e) {
       e.classList.add("open"), html.style.overflowX = "hidden", html.style.overflowY = "hidden";
     }
 
     t.addEventListener("click", function () {
-      r(e);
+      s(e);
     }), n.addEventListener("click", function () {
       i(e);
     }), e.addEventListener("click", function (t) {
@@ -249,10 +429,10 @@
       const t = e.querySelectorAll(".tabs-box .tab"),
             n = Array.prototype.slice.call(t),
             i = e.querySelectorAll(".services-places__content"),
-            r = Array.prototype.slice.call(i);
+            s = Array.prototype.slice.call(i);
       n.forEach((e, t) => {
         e.addEventListener("click", function () {
-          n.forEach(e => e.classList.remove("tab-active")), e.classList.add("tab-active"), r.forEach(e => e.classList.remove("active")), r[t].classList.add("active");
+          n.forEach(e => e.classList.remove("tab-active")), e.classList.add("tab-active"), s.forEach(e => e.classList.remove("active")), s[t].classList.add("active");
         });
       });
     }
@@ -263,27 +443,29 @@
       const e = t.querySelectorAll(".tabs-box .tab"),
             n = Array.prototype.slice.call(e),
             i = t.querySelectorAll(".services-train__content-box"),
-            r = Array.prototype.slice.call(i);
+            s = Array.prototype.slice.call(i);
       n.forEach((e, t) => {
         e.addEventListener("click", function () {
-          n.forEach(e => e.classList.remove("tab-active")), e.classList.add("tab-active"), r.forEach(e => e.classList.remove("active")), r[t].classList.add("active");
+          n.forEach(e => e.classList.remove("tab-active")), e.classList.add("tab-active"), s.forEach(e => e.classList.remove("active")), s[t].classList.add("active");
         });
       });
     }
   };
 }, function (e, t, n) {
-  n(0), n(1), n(6), n(4), n(2), e.exports = n(3);
+  n(0), n(1), n(2), n(3), n(4), n(9), n(7), n(5), e.exports = n(6);
 }, function (e, t, n) {
   "use strict";
 
   n.r(t);
-  var i = n(2),
-      r = n(1),
-      s = n(3),
-      c = n(0),
-      o = n(4);
+  var i = n(5),
+      s = n(4),
+      c = n(6),
+      r = n(0),
+      o = n(7),
+      a = n(3),
+      l = n(1),
+      d = n(2);
   document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("header");
-    Object(i.default)(), Object(r.default)(), Object(s.default)(), Object(c.default)(), Object(o.default)();
+    Object(i.default)(), Object(s.default)(), Object(c.default)(), Object(r.default)(), Object(o.default)(), Object(a.default)(), Object(l.default)(), Object(d.default)();
   });
 }]);
